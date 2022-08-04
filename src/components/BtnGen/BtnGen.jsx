8762670da5570aa1b9d0c250a67a778e473c6eca
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-04 12:46:55 -0300

    arrumei bug component btn

--- src/components/BtnGen/BtnGen.jsx
+++ src/components/BtnGen/BtnGen.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const BtnGen = ({text}) => {
+const BtnGen = ({text, onclick}) => {
   return (
-    <button>
+    <button onClick={onclick}>
      {text} 
     </button>
   )
